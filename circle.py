a2f8c6ff6c2b8faee1de5e66dd4f51a90883517c
DevKronos <aleshka.kotkov@yandex.ru>
2025-10-17 10:54:19 +0300

    Lab2

--- circle.py
+++ circle.py
@@ -2,9 +2,10 @@ import math
 
 
 def area(r):
+    '''Принимает радиус r, возвращает площадь круга с радиусаом r'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает радиус r, возвращает длину окружности с радиусом r'''
     return 2 * math.pi * r
-
